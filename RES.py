e37b5a6008e3c079ea38bba16b13023ac41dfe06
Hamed AMIRI <amiiri.hamed@gmail.com>
2024-02-21 16:34:06 +0100

    add RES.py file

---
+++ RES.py
@@ -0,0 +1,8 @@
+''''
+This script runs representative elementary size (RES) analysis on a binary 2D image.
+
+it takes path to the 2D tif file and a list of image sizes, and number of random samples as inputs to caluclate:
+two-point correlation function (S2) and its scaled version  (F2) in 2D.
+These values can then be analyzed for RES calculation for that image. 
+
+'''
